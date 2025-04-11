0ef73468375fa8442c6e1ac15b180e930bd97a22
olabola <leolabola@gmail.com>
2025-04-12 02:06:08 +1200

    fish and nvim config

---
+++ dotfiles/fish/config.fish
@@ -0,0 +1,25 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+status --is-interactive; and rbenv init - fish | source
+
+set -g fish_greeting
+fnm env --use-on-cd --shell fish | source
+zoxide init fish --cmd cd | source
+fzf --fish | source
+
+# git aliases
+alias gco='git checkout'
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gcane='git commit --amend --no-edit'
+alias gw='git switch'
+alias gw-='git switch -'
+alias gl='git log --oneline'
+alias gpf='git push --force-with-lease'
+alias gf='git fetch -v -p'
+alias gcm='git commit --message'
+alias gbs='git --no-pager diff --stat develop..HEAD'
