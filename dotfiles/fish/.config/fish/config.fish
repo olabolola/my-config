04f12ccd04013c42b511689694269d7fa7758d35
Salih <salih.jasim@cresconet.com>
2025-04-14 11:10:02 +1200

    merge fish configs

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -1,18 +1,70 @@
+# ==============================
+# Interactive Session Checks
+# ==============================
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # zoxide
+    if type -q zoxide
+        zoxide init fish --cmd cd | source
+    end
+
+    # fzf
+    if type -q fzf
+        fzf --fish | source
+    end
+
+    # direnv
+    if type -q direnv
+        direnv hook fish | source
+    end
 end
 
+# ==============================
+# Homebrew Environment
+# ==============================
 eval "$(/opt/homebrew/bin/brew shellenv)"
-status --is-interactive; and rbenv init - fish | source
 
-set -g fish_greeting
-fnm env --use-on-cd --shell fish | source
-zoxide init fish --cmd cd | source
-fzf --fish | source
+# ==============================
+# Runtime Environment Managers
+# ==============================
 
-direnv hook fish | source
+# rbenv
+if status is-interactive
+    if type -q rbenv
+        rbenv init - fish | source
+    end
+end
 
-# git aliases
+# pyenv
+set -Ux PYENV_ROOT $HOME/.pyenv
+set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+if status is-interactive
+    if type -q pyenv
+        pyenv init - | source
+    end
+end
+
+# fnm (Node.js)
+if type -q fnm
+    fnm env --use-on-cd --shell fish | source
+end
+
+# ==============================
+# Path Customizations
+# ==============================
+# pipx installs
+set -Ua PATH $HOME/.local/bin
+
+# Docker CLI path
+set -gx PATH /Applications/Docker.app/Contents/Resources/bin $PATH
+
+# ==============================
+# Appearance / UX
+# ==============================
+set -g fish_greeting  # Disable the default fish greeting
+
+# ==============================
+# Git Aliases
+# ==============================
 alias gco='git checkout'
 alias gs='git status'
 alias ga='git add'
@@ -25,3 +77,4 @@ alias gpf='git push --force-with-lease'
 alias gf='git fetch -v -p'
 alias gcm='git commit --message'
 alias gbs='git --no-pager diff --stat develop..HEAD'
+
