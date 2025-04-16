759b9adcd18d7c356f2e609ead952d33758d0865
olabola <leolabola@gmail.com>
2025-04-16 20:11:09 +1200

    more git config + formatting

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -4,6 +4,12 @@ vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Global default for all filetypes
+vim.opt.expandtab = true    -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4      -- Indent by 4 spaces
+vim.opt.tabstop = 4         -- A tab counts for 4 spaces
+vim.opt.softtabstop = 4     -- When hitting <Tab>, insert 4 spaces
+
 vim.api.nvim_create_autocmd('TextYankPost', {
 	desc = 'Highlight when yanking (copying) text',
 	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
