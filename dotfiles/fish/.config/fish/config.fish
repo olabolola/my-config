6a5d93da4cd41f7c8b1bed96abef2377d0169d46
olabola <leolaola@gmail.com>
2025-04-29 23:52:02 +1200

    vim mode in fish

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -21,6 +21,12 @@ if not pgrep -x skhd > /dev/null
     skhd --start-service
 end
 
+# Vim mode
+set -g fish_key_bindings fish_vi_key_bindings
+set fish_vi_force_cursor 1
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
 # ==============================
 # Runtime Environment Managers
 # ==============================
