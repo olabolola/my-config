5b7f8fe4a0feca97ae6511e6821efd674bb7c959
olabola <leolaola@gmail.com>
2025-05-01 23:01:48 +1200

    cool things

--- dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,33 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = { "lua", "python", "go", "javascript" , "groovy", "terraform", "sql" },
-      highlight = { enable = true },
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "gnn",
-          node_incremental = "grn",
-          node_decremental = "grm",
-        },
-      },
-    })
-  end
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+    config = function()
+        require('nvim-treesitter.configs').setup({
+            ensure_installed = { "lua", "python", "go", "javascript" , "groovy", "terraform", "sql" },
+            highlight = { enable = true },
+            indent = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "gnn",
+                    node_incremental = "grn",
+                    node_decremental = "grm",
+                },
+            },
+            textobjects = {
+                select = {
+                    enable=true,
+                    lookahead=true,
+                    keymaps = {
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                    },
+                },
+            },
+        })
+    end
 }
 
