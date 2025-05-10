f19e30b431c8609e3827fe24c962b822ef012f06
olabolola <leolabola@gmail.com>
2025-05-10 20:15:37 +1200

    colors

--- dotfiles/nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -1,6 +1,6 @@
 return {
   "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = { "nvim-lua/plenary.nvim",  },
   vim.keymap.set(
     "n", "]t",
     function() require("todo-comments").jump_next() end,
