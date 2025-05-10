28b84415470bcb063f53d9867b019b3f3700b809
olabolola <leolabola@gmail.com>
2025-05-10 20:22:41 +1200

    color

--- dotfiles/nvim/.config/nvim/lua/plugins/colors.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,20 +1,8 @@
-local function is_dark_mode()
-  local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
-  local result = handle:read("*a")
-  handle:close()
-  return result:match("Dark") ~= nil
-end
-
 return {
   "catppuccin/nvim",
   name="catppuccin",
   priority=1000,
   config = function()
-    local is_dark = is_dark_mode()
-    local flavour = is_dark and "mocha" or "latte"
-    require("catppuccin").setup({
-      flavour=flavour -- latte, frappe, macchiato, mocha
-    })
   vim.cmd.colorscheme("catppuccin")
   end,
 }
