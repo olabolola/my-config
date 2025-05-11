a283ee30362337341aea68c5f94f6cd80fced7e4
olabolola <leolabola@gmail.com>
2025-05-11 15:19:41 +1200

    log selected lines in neogit

--- dotfiles/nvim/.config/nvim/lua/plugins/neogit.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/neogit.lua
@@ -6,15 +6,24 @@ return {
     "nvim-telescope/telescope.nvim", -- optional
   },
   config = function()
-  require("neogit").setup({
-    integrations = {
-      diffview = True,
-    },
-  })
+    require("neogit").setup({
+      integrations = {
+        diffview = true,
+      },
+    })
 
-  vim.keymap.set("n", "<leader>gg", ":Neogit<CR>", {desc = "Open Neogit"})
-  vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>", {desc = "Open diff view"})
-  vim.keymap.set("n", "<leader>gl", ":Neogit log<CR>", {desc = "Open Neogit log"})
-  vim.keymap.set("n", "<leader>gp", ":Neogit push<CR>", {desc = "Neogit push"})
-end,
+    vim.keymap.set("n", "<leader>gg", ":Neogit<CR>", {desc = "Open Neogit"})
+    vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>", {desc = "Open diff view"})
+    vim.keymap.set("n", "<leader>gl", ":Neogit log<CR>", {desc = "Open Neogit log"})
+    vim.keymap.set("n", "<leader>gp", ":Neogit push<CR>", {desc = "Neogit push"})
+
+    -- View git log for selected lines
+    vim.keymap.set("v", "<leader>gl", function()
+      local file = vim.fn.expand("%")
+      vim.cmd([[execute "normal! \<ESC>"]])
+      local line_start = vim.fn.getpos("'<")[2]
+      local line_end = vim.fn.getpos("'>")[2]
+      require("neogit").action("log", "log_current", { "-L" .. line_start .. "," .. line_end .. ":" .. file })()
+    end, { desc = "Neogit Log for this range"})
+  end,
 }
