f23edb9061823e32d17647a5fa76fed584dd21f5
olabolola <leolabola@gmail.com>
2025-05-15 00:47:43 +1200

    cleanup

--- dotfiles/nvim/.config/nvim/lua/plugins/lualine.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("lualine").setup({
       options = { theme = "iceberg" },
