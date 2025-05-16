4f46aaa818bd609b9b28c394bec69288a07e6864
olabolola <leolabola@gmail.com>
2025-05-17 01:56:14 +1200

    better oil

--- dotfiles/nvim/.config/nvim/lua/plugins/oil.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/oil.lua
@@ -2,14 +2,16 @@ return {
   "stevearc/oil.nvim",
   opts = {
     default_file_explorer = true,
+    skip_confirm_for_simple_edits = true,
     view_options = {
       show_hidden = true,
+      natural_order = "fast",
     },
     columns = { "icon" },
   },
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
-    { "-", "<cmd>Oil<CR>", desc = "Open parent directory in Oil" },
+    { "-", "<cmd>Oil --float<CR>", desc = "Open parent directory in Oil" },
   },
 }
 
