0ae85a74f398d0c1095e3867c6089658914358af
olabolola <leolabola@gmail.com>
2025-05-18 17:43:37 +1200

    folding

---
+++ dotfiles/nvim/.config/nvim/lua/plugins/ufo.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    config = function()
+      -- General folding settings
+      vim.o.foldcolumn = '1'
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      -- Key mappings for opening and closing all folds
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+      -- Option 1: LSP client with native Neovim LSP
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true
+      }
+
+      require('ufo').setup()
+
+      -- Option 2: Treesitter as the main folding provider
+      require('ufo').setup({
+        provider_selector = function(bufnr, filetype, buftype)
+          return { 'treesitter', 'indent' }
+        end
+      })
+    end,
+  },
+}
+
