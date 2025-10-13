aad94e1d58836da599d5619b6af1daa2a3e45be2
olabolola <leolabola@gmail.com>
2025-10-14 11:23:10 +1300

    asd

--- dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
   build = ':TSUpdate',
   config = function()
     require('nvim-treesitter.configs').setup({
-      ensure_installed = { "lua", "python", "go", "javascript" , "typescript", "groovy", "terraform", "sql", "c", "vim", "markdown", "markdown_inline" },
+      ensure_installed = { "lua", "python", "go", "javascript" , "typescript", "groovy", "terraform", "sql", "c", "vim", "markdown", "markdown_inline", "rust" },
       sync_install = false,
       auto_install = true,
       ignore_install = {},
