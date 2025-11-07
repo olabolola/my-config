54a41e612f403672327f98a831c12e8f4f5fc75f
olabolola <leolabola@gmail.com>
2025-11-07 22:10:43 +1300

    rename

---
+++ dotfiles/nvim/.config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,16 @@
+return {
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",         -- required
+        "sindrets/diffview.nvim",        -- optional - Diff integration
+
+        -- Only one of these is needed.
+        "nvim-telescope/telescope.nvim", -- optional
+        "ibhagwan/fzf-lua",              -- optional
+        "nvim-mini/mini.pick",           -- optional
+        "folke/snacks.nvim",             -- optional
+    },
+    keys = {
+        {"<leader>gs", "<cmd>Neogit<cr>", desc = "Neogit"}
+    }
+}
